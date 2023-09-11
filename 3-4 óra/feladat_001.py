ec670991a71e1f54811b829f256bea9154e370b2
PleszkoLaccci <118164191+PleszkoLaccci@users.noreply.github.com>
2023-09-11 14:01:07 +0200

    Add files via upload

---
+++ 3-4 óra/feladat_001.py
@@ -0,0 +1,17 @@
+# feladat 001
+"""
+Kérjünk be a billentyűzetről a nevünket és írassuk ki a képernyőre! 
+"""
+
+"""
+
+nev = input("Kérem a nevedet! ")
+print(f"A megadott név a következő: {nev}")
+
+"""
+
+vezeteknev = input("Kérem a vezetéknevedet ")
+keresztnev = input("Kérem a keresztnevedet ")
+print(f"Szóval a neved {vezeteknev} {keresztnev}")
+
+
