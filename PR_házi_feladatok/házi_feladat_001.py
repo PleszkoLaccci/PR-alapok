b1a075046fe6002f278ad0678de881d4befc7c90
PleszkoLaccci <118164191+PleszkoLaccci@users.noreply.github.com>
2023-09-22 20:41:00 +0200

    Add files via upload

---
+++ PR_házi_feladatok/házi_feladat_001.py
@@ -0,0 +1,9 @@
+##házi_feladat
+
+válasz = input("Jó volt a napod? ")
+if válasz == "igen":
+    print(f"Örülök, hogy jó napod volt!")
+elif válasz == "nem":
+    print(f"Az nem jó, és miért nem volt jó?")
+else:
+    print(f"Sajnos nem értem a válaszod, légyszíves válaszolj nemmel vagy igennel!")
