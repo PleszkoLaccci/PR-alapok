f8aef48f4b1ad7175449b99cbf1004108dca51b4
PleszkoLaccci <118164191+PleszkoLaccci@users.noreply.github.com>
2023-10-09 14:14:55 +0200

    Add files via upload

---
+++ feladatok/feladat_012.py
@@ -0,0 +1,23 @@
+# be: pozitiv egesz szam
+# kiir: a szam paros vagy sem
+
+szam = int(input(f"Kérek egy egész számot: "))  
+
+"""
+if szam % 2 == 0:
+    print(f"A számod {szam} páros!")
+else:
+    print(f"A számod {szam} páratlan!"s)
+"""
+"""
+if szam % 2 == 0:
+    print(f"A számod {szam} páros!")
+elif szam % 2 == 1:
+    print(f"A számod {szam} páratlan!")
+"""
+"""
+if szam % 2 == 0:
+    print(f"A számod {szam} páros!")
+elif szam % 2 != 0:
+    print(f"A számod {szam} páratlan!")
+"""
---
+++ feladatok/feladat_013.py
@@ -0,0 +1,44 @@
+#be: szam
+#ki: a szam negativ
+
+
+"""
+szam = int(input("Adj meg egy számot! "))
+if szam < 0:
+    print(f"A megadott szám {szam} negatív.")
+print(f">> A program vége <<")
+"""
+
+#be: szam
+#ki: a szam negativ vagy nem negativ
+"""
+szam = int(input('Adj meg egy számot! '))
+if szam < 0:
+      print(f'A megadott szám {szam} negatív.')
+else:
+      print(f'A megadott szám nem negatív.')
+print('>> A program vége! <<')
+"""
+#be: szam
+#ki: a szam negativ vagy a szam pozitiv vagy a 0
+
+"""
+szam = int(input('Adj meg egy számot! '))
+if szam < 0:
+      print(f'A megadott szám {szam} negatív.')
+elif szam == 0:
+      print(f'A megadott szám {szam} nulla.')
+else:
+      print(f'A megadott szám {szam} pozitív.')
+print('>> A program vége! <<')
+"""
+
+szam = int(input('Adj meg egy számot! '))
+if szam < 0:
+      print(f'A megadott szám {szam} negatív.')
+elif szam == 0:
+      print(f'A megadott szám {szam} nulla.')
+else:
+      print(f'A megadott szám {szam} pozitív.')
+print('>> A program vége! <<')
+
