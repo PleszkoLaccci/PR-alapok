f686e2eaf0718e7fd4b018af6ad64e9be6bd7b94
PleszkoLaccci <118164191+PleszkoLaccci@users.noreply.github.com>
2023-10-09 14:15:24 +0200

    Add files via upload

---
+++ feladatok/feladat_011.py
@@ -0,0 +1,28 @@
+szam1 = int(input("Kérem adj meg egy számot! ")) 
+szam2 = int(input("Kérem adj meg egy másik számot! ")) 
+
+"""
+if szam1 < szam2:
+    print("szam1 kisebb mint szam2")
+elif szam2 < szam1:
+    print("szam2 kisebb mint szam1")
+else:
+    print("szam1 egyenlő szam2-vel")
+"""
+"""
+if szam1 < szam2:
+    print("szam1 kisebb mint szam2")
+if szam2 < szam1:
+    print("szam2 kisebb mint szam1")
+if szam1 == szam2:
+    print("szam1 egyenlő szam2-vel")
+"""
+
+"""
+if szam1 < szam2:
+    print("szam1 kisebb mint szam2")
+elif szam2 < szam1:
+    print("szam2 kisebb mint szam1")
+elif szam1 == szam2:
+    print("szam1 egyenlő szam2-vel")
+"""
