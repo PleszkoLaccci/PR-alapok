fb3595ede58ed0f2b5a5908d3bbde66723777b2b
PleszkoLaccci <118164191+PleszkoLaccci@users.noreply.github.com>
2023-10-15 21:42:27 +0200

    Add files via upload

---
+++ PR_házi_feladatok/házi_feladat_002.py
@@ -0,0 +1,29 @@
+##házi_feladat_002
+"""
+import math
+sugar = int(input("Kérem adja meg a kör sugarát: "))
+terulet = math.pi * sugar**2
+print(f"A kör területe sugar {sugar}-cal: {terulet}")
+"""
+
+
+"""
+import random
+
+tipp = int(input(f"Gondoltam egy számra 1 és 5 között, találd ki: "))
+gondoltszam = random.randint (1,5)
+if tipp == gondoltszam:
+    print("Ügyes vagy, kitaláltad!")
+else:
+    print("Nem sikerült, majd legközelebb!")
+"""
+
+"""
+kerdes = input(f"Jó napod volt ma? ")
+if kerdes == "igen":
+    print("Az fasza")
+elif kerdes == "nem":
+    print("Az nem jó!")
+else:
+    print("Nem értem a válaszod, kérem igen vagy nemmel válaszolj! ")
+"""
