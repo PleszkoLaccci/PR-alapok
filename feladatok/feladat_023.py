2a5aa333ff759e5776aa4cbc8cf6d1650237dd97
PleszkoLaccci <118164191+PleszkoLaccci@users.noreply.github.com>
2023-11-20 14:18:58 +0100

    Add files via upload

---
+++ feladatok/feladat_023.py
@@ -0,0 +1,7 @@
+honapok = ["januar", "februar", "marcius", "aprilis", "majus"]
+#lista = [1,2,3,4,5,6,7,8,9]
+print(honapok)
+#print(("asd").join(honapok))
+print(len(honapok))
+print(honapok[1])
+print(f"A lista utolsó legnagyobb indexe: {len(honapok)-1}")
